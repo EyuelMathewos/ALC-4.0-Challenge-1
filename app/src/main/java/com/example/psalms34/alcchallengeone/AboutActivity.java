272371b601876b05e40e31b6d278803a95e4c616
EyuelMathewos <eyuelmathewos@gmail.com>
2019-07-13 14:34:09 +0300

    Resolved SSL Security issue

--- app/src/main/java/com/example/psalms34/alcchallengeone/AboutActivity.java
+++ app/src/main/java/com/example/psalms34/alcchallengeone/AboutActivity.java
@@ -1,8 +1,11 @@
 package com.example.psalms34.alcchallengeone;
 
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -11,6 +14,12 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
         WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("https://www.pluralsight.com/");
+        myWebView.loadUrl("https://andela.com/alc/");
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
     }
 }
